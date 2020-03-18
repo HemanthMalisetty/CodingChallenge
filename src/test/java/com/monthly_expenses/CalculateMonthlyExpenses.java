8fe68181050667b4c9166cadd41e32be6a5351fc
HemanthMalisetty <hemanth.vignanasudha@gmail.com>
2020-03-19 01:23:21 +0530

    Updated some code

--- src/test/java/com/monthly_expenses/CalculateMonthlyExpenses.java
+++ src/test/java/com/monthly_expenses/CalculateMonthlyExpenses.java
@@ -4,6 +4,7 @@ import com.util.calendar.MyCalendar;
 import com.util.excel_reading.ExcelReading;
 
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class CalculateMonthlyExpenses {
 
@@ -18,20 +19,22 @@ public class CalculateMonthlyExpenses {
          * Example : For march'2020
          * 03/2020
          */
-        String monthYear = "03/2020";
+        System.out.println("Enter subscription month and Year in below format and press enter\n" + "Example : 03/2020");
+        Scanner sc = new Scanner(System.in);
+        String monthYear = sc.nextLine();
+
+        System.out.println("Enter no of papers you want to subscribe and press enter");
+        int papersCount = sc.nextInt();
 
         /**
          * Add newspaper names in the array
          */
-        String[] newspaperNames = {"Hindu"}; //91.0
-        //String[] newspaperNames = {"TOI"}; //116.0
-        //String[] newspaperNames = {"ET"}; //102.0
-        //String[] newspaperNames = {"bm"};//46.5
-        //String[] newspaperNames = {"ht"}; //80.0
-        //String[] newspaperNames = {"TOI", "Hindu"}; //207.0
-        //String[] newspaperNames = {"TOI", "Hindu", "ET"};
-        //String[] newspaperNames = {"TOI", "Hindu", "ET", "bm"};
-        //String[] newspaperNames = {"TOI", "Hindu", "ET", "bm", "Ht"};
+        System.out.println("Enter newspaper names in new line (press enter after entering each newspaper name)\n" + "Ex:\n" + "toi\n" + "Hindu");
+        String[] newspaperNames = new String[papersCount];
+        for (int i = 0 ; i < papersCount; i++){
+            newspaperNames[i] = sc.next();
+        }
+        System.out.println("Calculating the monthly expenses please wait.....");
 
         /**
          * Getting the last date of the current month
